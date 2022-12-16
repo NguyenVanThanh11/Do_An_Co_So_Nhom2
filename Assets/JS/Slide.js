04851e93413f6ee68216204764a986c2df99a927
NguyenVanThanh <20010798@st.phenikaa-uni.edu.vn>
2022-12-16 11:11:09 +0700

    FE-Finish

--- Assets/JS/Slide.js
+++
@@ -1,8 +0,0 @@
-var counter  = 1;
-setInterval(function() {
-    document.getElementById("home__slide-radio" + counter).checked = true;
-    counter++;
-    if(counter > 4) {
-        counter = 1;
-    }
-}, 3000);
--- README.md
+++
@@ -1,2 +0,0 @@
-# Do_An_Co_So_Nhom2
- Du An web ban cay canh
